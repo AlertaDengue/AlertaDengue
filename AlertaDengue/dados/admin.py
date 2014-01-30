de81610a65671e3a83dcbd3d95139240f47ac497
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-01-30 13:15:42 -0200

    consertei erros

--- AlertaDengue/dados/admin.py
+++ AlertaDengue/dados/admin.py
@@ -3,7 +3,4 @@ from dados.models import Dengue_2010, Dengue_2011, Dengue_2012, Dengue_2013, Den
 
 for mod in [Dengue_2010, Dengue_2011, Dengue_2012, Dengue_2013, DengueConfirmados_2013]:
     admin.site.register(mod, admin.GeoModelAdmin)
-x=10
-media_reputacao=10
-reputacao_mais_baixa=2
-(x > ((media_reputacao - reputacao_mais_baixa)/2. + reputacao_mais_baixa) or x > (reputacao_mais_alta - (reputacao_mais_alta - media_reputacao)/2.))
+
