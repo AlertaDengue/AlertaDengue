1b63dfc1f7ab77d9ade96af05949e11fb086e62a
Flávio Codeço Coelho <fccoelho@gmail.com>
2014-10-27 11:13:04 -0200

    Added filters to the interface to show content depending on authentication status

--- AlertaDengue/AlertaDengue/urls.py
+++ AlertaDengue/AlertaDengue/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from dados.views import HomePageView, SinanCasesView, AboutPageView, ContactPageView, MapaDengueView, MapaMosquitoView, HistoricoView, AlertaPageView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 
 admin.autodiscover()
 
@@ -12,10 +13,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'AlertaDengue.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', AlertaPageView.as_view(), name='alerta'),
-    url(r'^alerta-detalhado/$', HomePageView.as_view(), name='home'),
+    url(r'^alerta-detalhado/$', login_required(HomePageView.as_view()), name='home'),
     url(r'^mapadengue/$', MapaDengueView.as_view(), name='mapadengue'),
     url(r'^mapamosquito/$', MapaMosquitoView.as_view(), name='mapamosquito'),
-    url(r'^historico/$', HistoricoView.as_view(), name='historico'),
+    url(r'^historico/$', login_required(HistoricoView.as_view()), name='historico'),
     url(r'^informacoes/$', AboutPageView.as_view(), name='about'),
     url(r'^contato/$', ContactPageView.as_view(), name='contact'),
     url(r'^sinan/(\d{4})/(\d{1,2})', SinanCasesView.as_view(), name='sinan'),
