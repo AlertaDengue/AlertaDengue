08269431de3924bbd1421a4169c3f38e3864e970
Israel Teixeira <israelst@gmail.com>
2015-06-05 18:37:45 -0300

    Cover "alerta"

--- AlertaDengue/dados/tests.py
+++ AlertaDengue/dados/tests.py
@@ -7,9 +7,9 @@ from django.test import TestCase
 class TestAlertaPageView(TestCase):
     def setUp(self):
         self.url = reverse('alerta')
+        self.response = self.client.get(self.url)
 
     def test_casos_por_ap(self):
-        response = self.client.get(self.url)
         casos_por_ap = {
             u"1.0": 0,
             u"4.0": 5,
@@ -22,4 +22,21 @@ class TestAlertaPageView(TestCase):
             u"5.1": 6,
             u"3.3": 55,
         }
-        self.assertEqual(json.loads(response.context['casos_por_ap']), casos_por_ap)
+        self.assertEqual(json.loads(self.response.context['casos_por_ap']), casos_por_ap)
+
+    def test_alerta(self):
+        alerta = {
+             1.0: 1,
+             2.1: 1,
+             2.2: 1,
+             3.1: 2,
+             3.2: 1,
+             3.3: 2,
+             4.0: 1,
+             5.1: 1,
+             5.2: 1,
+             5.3: 1
+         }
+
+        self.assertEqual(self.response.context['alerta'], alerta)
+
