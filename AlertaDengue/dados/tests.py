4a338f3ce843a941fcb464fd39d51be1a342300e
Israel Teixeira <israelst@gmail.com>
2015-06-05 18:33:22 -0300

    Cover "casos por ap"

--- AlertaDengue/dados/tests.py
+++ AlertaDengue/dados/tests.py
@@ -1,4 +1,25 @@
 # coding=utf-8
+import json
+
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+class TestAlertaPageView(TestCase):
+    def setUp(self):
+        self.url = reverse('alerta')
+
+    def test_casos_por_ap(self):
+        response = self.client.get(self.url)
+        casos_por_ap = {
+            u"1.0": 0,
+            u"4.0": 5,
+            u"2.2": 4,
+            u"3.2": 6,
+            u"3.1": 40,
+            u"5.3": 1,
+            u"2.1": 18,
+            u"5.2": 8,
+            u"5.1": 6,
+            u"3.3": 55,
+        }
+        self.assertEqual(json.loads(response.context['casos_por_ap']), casos_por_ap)
