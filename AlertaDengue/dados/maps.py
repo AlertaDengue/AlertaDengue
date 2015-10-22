6c9e38d34be6d7a0b3e29fde5fd832566591e4d8
Flávio Codeço Coelho <fccoelho@gmail.com>
2015-10-22 12:42:23 -0200

    uncommented connection  creation

--- AlertaDengue/dados/maps.py
+++ AlertaDengue/dados/maps.py
@@ -2,11 +2,11 @@ import psycopg2
 from psycopg2.extras import DictCursor
 from django.conf import settings
 import geojson
-from shapely.geometry import shape
 
-# conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='aldengue'".format(settings.PSQL_DB,
-#                                                                                      settings.PSQL_USER,
-#                                                                                      settings.PSQL_HOST))
+
+conn = psycopg2.connect("dbname='{}' user='{}' host='{}' password='aldengue'".format(settings.PSQL_DB,
+                                                                                     settings.PSQL_USER,
+                                                                                     settings.PSQL_HOST))
 
 
 def get_city_geojson(municipio):
@@ -22,12 +22,12 @@ def get_city_geojson(municipio):
     cur.execute('select geocodigo, nome, geojson, populacao, uf from "Dengue_global"."Municipio" where geocodigo=%s', (municipio,))
     datum = cur.fetchone()
     feat = geojson.loads(datum['geojson'])
-    #shapeobj = shape(feat)
+
     feat['type'] = 'Feature'
-    #feat['geometry']['type'] = 'Polygon'
+
     feat['properties'] = {'geocodigo': datum['geocodigo'], 'nome': datum['nome'], 'populacao': datum['populacao']}
 
     geoj = geojson.loads(head + geojson.dumps(feat) + tail)
-    #print(geoj)
+
     return geoj
 
