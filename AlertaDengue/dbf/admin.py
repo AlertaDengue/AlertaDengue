a4fd4b0caa21cc98f07209f41a6f48ee2b37174e
Flavio Amieiro <amieiro.flavio@gmail.com>
2016-09-19 16:37:42 -0300

    Adds simple DBF model

--- AlertaDengue/dbf/admin.py
+++ AlertaDengue/dbf/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from dbf.models import DBF
 
-# Register your models here.
+admin.site.register(DBF)
--- AlertaDengue/dbf/models.py
+++ AlertaDengue/dbf/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+def current_year():
+    return date.today().year
+
+class DBF(models.Model):
+    file = models.FileField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    export_date = models.DateField()
+    notification_year = models.IntegerField(default=current_year)
