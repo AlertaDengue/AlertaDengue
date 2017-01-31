251d04657d1cc103d3b73c33e724d96ac7f74079
Flavio Amieiro <amieiro.flavio@gmail.com>
2017-01-31 00:41:57 -0200

    Sends e-mail to user when import process finishes

--- AlertaDengue/AlertaDengue/settings.py
+++ AlertaDengue/AlertaDengue/settings.py
@@ -172,6 +172,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = config('MEDIA_ROOT', default='')
 
 EMAIL_BACKEND = config('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_FROM_ADDRESS = config('EMAIL_FROM_ADDRESS', 'no-reply@info.dengue.mat.br')
 
 if EMAIL_BACKEND != 'django.core.mail.backends.console.EmailBackend':
     EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD = config('EMAIL_CONFIG', default='example_host,25,username,password', cast=Csv())
--- AlertaDengue/dbf/tasks.py
+++ AlertaDengue/dbf/tasks.py
@@ -1,8 +1,31 @@
+from django.conf import settings
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from celery import shared_task
 
 from dbf.models import DBF
 from dbf.sinan import Sinan
+
+
+def send_success_email(dbf):
+    subject = "[InfoDengue] DBF enviado em {:%d/%m/%Y} importado com successo".format(
+            dbf.uploaded_at)
+    body = render_to_string("successful_import_email.txt",
+            context={"dbf": dbf})
+
+    send_mail(subject, body, settings.EMAIL_FROM_ADDRESS,
+                [dbf.uploaded_by.email])
+
+def send_failure_email(dbf, message):
+    subject = "[InfoDengue] Falha ao importar DBF enviado em {:%d/%m/%Y}".format(
+            dbf.uploaded_at)
+    body = render_to_string("failed_import_email.txt",
+            context={"dbf": dbf, "error_message": message})
+
+    send_mail(subject, body, settings.EMAIL_FROM_ADDRESS,
+                [dbf.uploaded_by.email])
+
 
 @shared_task
 def import_dbf_to_database(dbf_id):
@@ -12,6 +35,6 @@ def import_dbf_to_database(dbf_id):
         # have to give it a file path and it will read from the disc.
         sinan = Sinan(dbf.file.path, dbf.notification_year)
         sinan.save_to_pgsql()
+        send_success_email(dbf)
     except ValidationError as exc:
-        error_message = exc.message
-        print(error_message)
+        send_failure_email(dbf, exc.message)
