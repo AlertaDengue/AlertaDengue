90e435dc3eee84e6ef4ee22afda95c18a21ae11a
Flavio Amieiro <amieiro.flavio@gmail.com>
2017-02-05 21:30:01 -0200

    Stops writting gunicorn logs to disk
    We can use `docker logs` to see the output of gunicorn

--- runwsgi.sh
+++ runwsgi.sh
@@ -6,4 +6,4 @@ WSGI_PORT="8000"
 ERRLOG="$DEPLOY_HOME/logs/wsgi_server.err"
 ACCESS_LOG="$DEPLOY_HOME/logs/wsgi_server.access"
 
-exec gunicorn -w $NUM_WORKERS -b $WSGI_HOST:$WSGI_PORT --error-logfile=$ERRLOG --access-logfile=$ACCESS_LOG AlertaDengue.wsgi:application --timeout 240
+exec gunicorn -w $NUM_WORKERS -b $WSGI_HOST:$WSGI_PORT AlertaDengue.wsgi:application --timeout 240
