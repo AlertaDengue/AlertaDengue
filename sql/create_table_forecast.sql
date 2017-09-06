93510d6dfe876df40c3b3375dc9f6ad5fd3c04f7
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2017-09-06 19:25:08 -0400

    Resolves #116

---
+++ sql/create_table_forecast.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "Municipio".forecast (
+    epiweek SMALLINT NOT NULL,
+    geoid INT NOT NULL,
+    cases INT NOT NULL,
+    model_id VARCHAR(128) NOT NULL,
+    commit_id CHAR(7) NOT NULL,
+    PRIMARY KEY (epiweek, geoid)
+)
