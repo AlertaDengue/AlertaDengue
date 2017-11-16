e9ac36ddec385cec532cee2a8743e903a23a21d5
Flavio Amieiro <amieiro.flavio@gmail.com>
2017-11-16 17:45:07 +0100

    Removes trailing whitespace

--- AlertaDengue/manager/router.py
+++ AlertaDengue/manager/router.py
@@ -5,12 +5,12 @@ class DatabaseAppsRouter(object):
     """
     A router to control all database operations on models for different
     databases.
- 
+
     In case an app is not set in settings.DATABASE_APPS_MAPPING, the router
     will fallback to the `default` database.
- 
+
     Settings example:
- 
+
     DATABASE_APPS_MAPPING = {'app1': 'db1', 'app2': 'db2'}
     """
 
@@ -20,14 +20,14 @@ class DatabaseAppsRouter(object):
         if model._meta.app_label in settings.DATABASE_APPS_MAPPING:
             db = settings.DATABASE_APPS_MAPPING[model._meta.app_label]
         return db
- 
+
     def db_for_write(self, model, **hints):
         """Point all write operations to the specific database."""
         db = None
         if model._meta.app_label in settings.DATABASE_APPS_MAPPING:
             db = settings.DATABASE_APPS_MAPPING[model._meta.app_label]
         return db
- 
+
     def allow_relation(self, obj1, obj2, **hints):
         """Allow any relation between apps that use the same database."""
         db_obj1 = settings.DATABASE_APPS_MAPPING.get(obj1._meta.app_label)
@@ -39,7 +39,7 @@ class DatabaseAppsRouter(object):
             allow = True if db_obj1 == db_obj2 else False
 
         return allow
- 
+
     def allow_syncdb(self, db, model):
         """Make sure that apps only appear in the related database."""
         allow = None
