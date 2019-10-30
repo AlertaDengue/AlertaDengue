77b0cebb0b78d4dfc6419d2c2a8b2bbab764c01a
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-10-30 14:55:35 -0400

    Fixing postgis docker

--- docker/postgres_setup/entrypoint.sh
+++ docker/postgres_setup/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-
-./docker-entrypoint-initdb.d/01-alter_password.sh
-./docker-entrypoint-initdb.d/01-create_dengue_db.sh
-./docker-entrypoint-initdb.d/01-create_infodengue_db.sh
+echo "[II] Starting entrypoint scripts ..."
+echo "[II] postgis.sh ..."
+# . /docker-entrypoint-initdb.d/postgis.sh
+echo "[II] 01-alter_password.sh ..."
+# . /docker-entrypoint-initdb.d/01-alter_password.sh
+echo "[II] 01-create_dengue_db.sh ..."
+# . /docker-entrypoint-initdb.d/01-create_dengue_db.sh
+echo "[II] 01-create_infodengue_db.sh ..."
+# . /docker-entrypoint-initdb.d/01-create_infodengue_db.sh
+echo "[II] done."
