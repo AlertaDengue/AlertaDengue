1e88c35fe529b94062deeb8eca2dd3e085f7f60e
Sandro Loch <es.loch@gmail.com>
2020-01-10 15:25:05 -0300

    Removed from__future__ encoding python2x

--- AlertaDengue/AlertaDengue/celery.py
+++ AlertaDengue/AlertaDengue/celery.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# absolute_import makes shure this module won't clash with the library
-from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
