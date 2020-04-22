e09dca9bad8c2cba0e77c49231d34e142b8e0807
Sandro Loch <es.loch@gmail.com>
2020-04-22 17:39:11 -0300

    Fixes dbf load to save into media_root (#327)
    * Fixes access permissions into MEDIA_ROOT
    
    * Modified gid in dockerfile

--- docker/prepare_permission.sh
+++ docker/prepare_permission.sh
@@ -5,6 +5,6 @@ if [-z ${HOST_UID}]
   then
     RUN useradd --shell=/bin/bash --home=/srv/deploy/ --create-home deploy
   else
-    groupadd --gid ${HOST_GID} celery # we need the same GID as the host.
+    groupadd --gid ${HOST_GID} deploy # we need the same GID as the host.
     useradd --shell=/bin/bash --home=/srv/deploy/ --create-home deploy -u ${HOST_UID} -g ${HOST_GID}
 fi;
