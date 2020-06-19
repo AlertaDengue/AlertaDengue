d71ddafb7a15fd7b0a388616cfa6c155d95eced2
Sandro Loch <es.loch@gmail.com>
2020-06-19 12:30:43 -0300

    Added step copy .env file (#340)
    * Added step copy .env file
    
    * Created into makefile filter for staging build
    
    * Remove mamba
    
    * Fixes permissions
    
    * Add step for checking docker version
    
    * Force install docker
    
    * Bugfix prepare_permissions script
    
    * Pinned django version in requirements
    
    * Insert flag --no-deps to flake8_staging in makefile
    
    * Modify env_example file
    
    * Create script environment activate
    
    * Add new step requirement-dev install
    
    * Change requirement-dev install
    
    * Create Dockerfile-dev
    
    * Change pip install in Dockerfile-dev
    
    * Add Entrypoint with activation script
    
    * Add prepare_permission script

--- docker/prepare_permission.sh
+++ docker/prepare_permission.sh
@@ -3,7 +3,7 @@
 # Basic if statement
 if [ -z ${HOST_UID} ]
   then
-    RUN useradd --shell=/bin/bash --home=/srv/deploy/ --create-home deploy
+    useradd --shell=/bin/bash --home=/srv/deploy/ --create-home deploy
   else
     groupadd --gid ${HOST_GID} deploy # we need the same GID as the host.
     useradd --shell=/bin/bash --home=/srv/deploy/ --create-home deploy -u ${HOST_UID} -g ${HOST_GID}
