de5d52d35c1ecb01aceb4995a112fa107bff3b96
Sandro Loch <es.loch@gmail.com>
2020-07-09 18:25:30 -0300

    Add new user for tests in .env_example file

--- docker/postgres_setup/01-alter_password.sh
+++ docker/postgres_setup/01-alter_password.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-echo "ALTER ROLE administrador WITH PASSWORD '""$PSQL_PASSWORD""';" | psql -v ON_ERROR_STOP=1 --username "$PSQL_USER"
-echo "ALTER ROLE forecast WITH PASSWORD '""$PSQL_PASSWORD""';" | psql -v ON_ERROR_STOP=1 --username "$PSQL_USER"
-echo "ALTER ROLE dengue WITH PASSWORD '""$PSQL_PASSWORD""';" | psql -v ON_ERROR_STOP=1 --username "$PSQL_USER"
+echo "ALTER ROLE administrador WITH PASSWORD '""$POSTGRES_PASSWORD""';" | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"
+echo "ALTER ROLE forecast WITH PASSWORD '""$POSTGRES_PASSWORD""';" | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"
