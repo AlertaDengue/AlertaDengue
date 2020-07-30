dc0ee43c89c4d5b34c1e0de7270745a230fa8d7d
Sandro Loch <es.loch@gmail.com>
2020-07-30 18:30:25 -0300

    Rename the module celery to celeryapp and  app AlertaDengue to ad_main (#351)

--- AlertaDengue/ad_main/celeryapp.py
+++ AlertaDengue/ad_main/celeryapp.py
@@ -2,11 +2,17 @@ import os
 
 from celery import Celery
 
+
 # This makes sure celery knows where settings are, so we don't need to set it
 # when calling it from the command line
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ad_main.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ad_main.settings')
 
-app = Celery("AlertaDengue")
+app = Celery(
+    "ad_main",
+    broker=os.getenv("CELERY_BROKER_URL"),
+    backend=os.getenv("CELERY_BACKEND"),  #
+    include=["dbf.tasks"],
+)
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
