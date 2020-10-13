4ac4939186575ecd86f80ff763d94fe9a9e06f72
Sandro Loch <es.loch@gmail.com>
2020-10-13 09:23:55 -0300

    Change order context processor definitions in context_processors

--- AlertaDengue/ad_main/settings.py
+++ AlertaDengue/ad_main/settings.py
@@ -100,13 +100,13 @@ TEMPLATES = [
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'OPTIONS': {
             'context_processors': [
-                "django.contrib.auth.context_processors.auth",
-                "django.template.context_processors.debug",
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
                 "django.template.context_processors.i18n",
                 "django.template.context_processors.media",
                 "django.template.context_processors.static",
                 "django.template.context_processors.tz",
-                "django.template.context_processors.request"
                 "django.contrib.messages.context_processors.messages",
                 "maintenance_mode.context_processors.maintenance_mode",
             ]
