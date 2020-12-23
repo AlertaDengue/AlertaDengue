9a13c2bb3ee4f7ff824da1034bdce6f433d48779
Sandro Loch <es.loch@gmail..com>
2020-12-23 06:32:27 -0300

    Restructure the Makefile

--- AlertaDengue/api/settings.py
+++ AlertaDengue/api/settings.py
@@ -1,12 +1,11 @@
 try:
     # to be used externally (such as notebooks science)
-    from AlertaDengue.AlertaDengue import settings
+    from ad_main import settings
 except Exception:
     from django.conf import settings
 
 PSQL_USER = settings.PSQL_USER
 PSQL_PASSWORD = settings.PSQL_PASSWORD
 PSQL_HOST = settings.PSQL_HOST
-PSQL_PORT = settings.PSQL_PORT
 PSQL_DB = settings.PSQL_DB
 PSQL_PORT = settings.PSQL_PORT
