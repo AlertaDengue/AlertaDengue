cddb05a448e3dc6bf1a5d6722f8923c190c7b698
Sandro Loch <es.loch@gmail.com>
2021-04-26 10:46:51 -0300

    Patch to release 1.0.1 (#425)
    * Fix warning default_auto_field for django3.2
    
    * Change forecast migrations
    
    * Minor changes

--- AlertaDengue/forecast/tests/test_models.py
+++ AlertaDengue/forecast/tests/test_models.py
@@ -1,21 +1,27 @@
 from django.test import TestCase
-from unittest import skip
 
 # local
 from forecast.models import ForecastModel
 
+# from forecast.models import ForecastCity
 
-@skip
+
 class ForecastModelTest(TestCase):
     multi_db = True
+    databases = ['forecast']
 
     @classmethod
     def setUpTestData(cls):
         ForecastModel.objects.create(
-            name='Model1', weeks=3, commit_id='1234567', active=True
-        )
+            name='Model1',
+            github='http://github.com',
+            commit_id='1234567',
+            train_start='2021-04-22',
+            train_end='2021-04-22',
+            filename='Trained model',
+            active=True,
+        ),
 
     def test_select(self):
         fm = ForecastModel.objects.get(name='Model1')
-
-        self.assertEqual(fm.weeks, 3)
+        self.assertEqual(fm.filename, 'Trained model')
