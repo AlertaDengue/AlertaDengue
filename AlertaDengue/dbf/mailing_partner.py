43785ebd1efba4c29ac520697cb78f641002dc14
Sandro Loch <es.loch@gmail.com>
2021-09-22 10:55:57 -0300

    Change template message in the mailing_partner module

--- AlertaDengue/dbf/mailing_partner.py
+++ AlertaDengue/dbf/mailing_partner.py
@@ -1,19 +1,18 @@
+import logging
 import os
-from dotenv import load_dotenv
-from os.path import join, dirname
 from datetime import datetime
+from email.mime.image import MIMEImage
+from os.path import dirname, join
 
 import django.core.mail
+import pandas as pd
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-import logging
-import pandas as pd
 
 from ad_main import settings
 from dados.episem import episem
-
 
 env_file = os.environ.get('ENV_FILE', '.env')
 
@@ -33,6 +32,7 @@ db_engine = create_engine(PSQL_URI)
 dt_now = datetime.now().strftime('%Y-%m-%d')
 year_week = episem(dt_now, sep='')
 year, week = year_week[:4], year_week[-2:]
+last_week = int(week) - 1
 
 
 def get_partner_active():
@@ -78,7 +78,6 @@ def send_email_partner(
     Given a datatuple of (subject, message, from_email, recipient_list), send
     each message to each recipient list. Return the number of emails sent.
     """
-
     mailing = get_partner_active()
 
     connection = get_connection()
@@ -87,19 +86,26 @@ def send_email_partner(
     messages = []
 
     for idx, row in mailing.iterrows():
-
-        subject = f'Informe de dados Infodengue SE{week}'
+        subject = f'Informe de dados Infodengue SE{last_week}'
         body = render_to_string(
             "email_secretarias.txt",
-            context={"name": row['contact'], "context_message": week},
+            context={"name": row['contact'], "context_message": last_week},
         )
-
         message = EmailMultiAlternatives(
             subject, body, mail_from, [row['email']]
         )
-
         logging.info(f"Enviando email para: {row['email']},")
         # message.attach_alternative(html, 'text/html')
+        fp = open(
+            os.path.join(
+                settings.STATICFILES_DIRS[0], 'img/logo_signature.png'
+            ),
+            'rb',
+        )
+        msg_img = MIMEImage(fp.read())
+        fp.close()
+        # msg_img.add_header('Content-ID', '<{}>'.format("logo_signature.png"))
+        message.attach(msg_img)
         messages.append(message)
 
     return connection.send_messages(messages)
