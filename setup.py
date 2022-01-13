ca13b72a4f29db2e129f8332b2ad6afff1cecf29
esloch <es.loch@gmail.com>
2022-01-13 15:22:44 -0300

    bumped version to 2.6.0

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def read(filename):
 
 setup(
     name='AlertaDengue',
-    version='2.5.2',
+    version='2.6.0',
     description="Dengue alert",
     long_description=readme + '\n\n' + history,
     author="FIOCRUZ",
