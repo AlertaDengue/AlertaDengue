62963046918e4d7c4129b5d20aab1a1c6d6bcbf4
SANRO LOCH <es.loch@gmail.com>
2022-03-10 11:28:46 -0300

    bumped version to 2.7.5

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def read(filename):
 
 setup(
     name='AlertaDengue',
-    version='2.7.4',
+    version='2.7.5',
     description="Dengue alert",
     long_description=readme + '\n\n' + history,
     author="FIOCRUZ",
