58ef636bea14e2d85c71a0b1fa6add5e38c95d9a
Sandro Loch <es.loch@gmail.com>
2022-05-09 21:22:08 -0300

    Update docker and compose files

--- AlertaDengue/runwsgi.sh
+++ AlertaDengue/runwsgi.sh
@@ -5,9 +5,10 @@ WSGI_PORT="8000"
 ERRLOG="/opt/services/logs/wsgi_server.err"
 ACCESS_LOG="/opt/services/logs/wsgi_server.access"
 
-echo "Collecting static files.."
-python3 manage.py collectstatic --noinput
 echo "Creating geofiles..."
 python3 manage.py sync_geofiles
+
+echo "Collecting static files.."
+python3 manage.py collectstatic --noinput
 
 exec gunicorn -w $NUM_WORKERS -b $WSGI_HOST:$WSGI_PORT ad_main.wsgi:application --timeout 9640
