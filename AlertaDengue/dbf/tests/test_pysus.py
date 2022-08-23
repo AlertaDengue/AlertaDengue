69e63080adbe63a7572212f7f6afffd3b8921f26
Sandro Loch <es.loch@gmail.com>
2022-08-23 07:27:31 -0300

    Add tests to load data from pysus

---
+++ AlertaDengue/dbf/tests/test_pysus.py
@@ -0,0 +1,53 @@
+from datetime import date, timedelta
+
+import numpy as np
+import pandas as pd
+from dbf.pysus import COL_TO_RENAME, PySUS, calc_birth_date
+from django.test import TestCase
+
+
+class Test_LoadPySUS(TestCase):
+    """_summary_"""
+
+    def test_get_data(self):
+        """
+        Check if dataframe contain the expected fields.
+        RESUL_PCR field is missing in PySUS data from ZIKA disease.
+        Param:
+            year: str
+            disease: str
+        Return: pd.DataFrame
+        """
+        year = "2016"
+        disease = "zika"
+        pysus = PySUS(year, disease)
+
+        df = pysus.get_data(year, disease)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertIn("resul_pcr", df.columns)
+        self.assertEquals(list(COL_TO_RENAME.values()), list(df.columns))
+
+    def test_calc_birth_date(self):
+        """
+        Check return of birthday date and dtypes are datetime.
+        DT_NASC field is missing in PySUS data.
+        The value is taken from DT_NOTIFIC field.
+        """
+
+        age_dec = 4043
+        value = date(2016, 3, 15)
+        days_x_year = 43 * 365
+        birth_fake = np.array(date(1973, 3, 26), dtype="object")
+        birthday = calc_birth_date(value, age_dec)
+        birth_date = value - timedelta(days=days_x_year)
+
+        # self.assertIsInstance(age_dec, np.float64)
+        self.assertIsInstance(value, date)
+        self.assertIsInstance(birth_date, date)
+        self.assertEqual(birthday, birth_fake)
+
+    def test_parse_dataframe(self):
+        pass
+
+    def test_save(self):
+        pass
