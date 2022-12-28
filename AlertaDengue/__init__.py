77215c5e5a9f41f06a4c0b79d0066ca271fbaf12
Sandro Loch <es.loch@gmail.com>
2022-12-28 10:50:39 -0300

    feat(release): Add semantic release dependencies

--- AlertaDengue/__init__.py
+++ AlertaDengue/__init__.py
@@ -1,1 +1,13 @@
-from .ad_main.version import __version__  # noqa: F401
+"""pyreaddbc Python package"""
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "3.2.0"  # changed by semantic-release
+
+
+version: str = get_version()
+__version__: str = version
