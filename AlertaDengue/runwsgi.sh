5985ec369073269644a66a795466d3b037c7ccaf
Sandro Loch <es.loch@gmail.com>
2023-01-05 11:15:38 -0300

    Fix deploy of staging_web

--- AlertaDengue/runwsgi.sh
+++ AlertaDengue/runwsgi.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
-DEPLOY_HOME="/opt/services/"
-NUM_WORKERS=4
-WSGI_HOST="0.0.0.0"
-WSGI_PORT="8000"
-ERRLOG="$DEPLOY_HOME/logs/wsgi_server.err"
-ACCESS_LOG="$DEPLOY_HOME/logs/wsgi_server.access"
+#!/usr/bin/env bash
 
-echo "Collecting static files.."
-python3 manage.py collectstatic --noinput
 # echo "Creating geofiles..."
 # python3 manage.py sync_geofiles
 
-exec gunicorn -w $NUM_WORKERS -b $WSGI_HOST:$WSGI_PORT ad_main.wsgi:application --timeout 9640
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
+
+echo "Starting gunicorn..."
+exec gunicorn -w 4 -b 0.0.0.0:8000 ad_main.wsgi:application --timeout 9640
