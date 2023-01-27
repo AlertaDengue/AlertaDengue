db796d303510fb7f6ccc2036b80a4fe75f33e238
Sandro Loch <es.loch@gmail.com>
2023-01-27 16:39:24 -0300

    feat(container): Initial refactore

--- containers/postgres/postgres_setup/02-dump_into_database.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-gunzip -c /dumps/latest_infodengue.sql.gz | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$PSQL_DBF"
-gunzip -c /dumps/latest_dengue.sql.gz | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$PSQL_DB"
