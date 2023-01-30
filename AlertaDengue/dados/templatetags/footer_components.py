795d8fe1ea175ae3b86637fade6adfff959814ce
Sandro Loch <es.loch@gmail.com>
2023-01-30 14:25:30 -0300

    feat(template) Add github sponsors card (#598)
    * feat(template): Add github sponsors card
    
    * chore(template): Create footer component
    
    * chore(template): Add css for footer component
    
    * chore(template): Create div container
    
    * chore(template): Add footer to the teampage

---
+++ AlertaDengue/dados/templatetags/footer_components.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag(
+    "components/main/footer.html", takes_context=True
+)
+def githubsponsor_component(context):
+    return context
