979d4f99ced10a35f09b88a1fdf2ccc3eeed0f1d
Sandro Loch <es.loch@gmail.com>
2023-01-31 08:16:32 -0300

    fix(pyproject): Update linting package versions (#601)

--- AlertaDengue/dados/templatetags/footer_components.py
+++ AlertaDengue/dados/templatetags/footer_components.py
@@ -3,8 +3,6 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag(
-    "components/main/footer.html", takes_context=True
-)
+@register.inclusion_tag("components/main/footer.html", takes_context=True)
 def githubsponsor_component(context):
     return context
