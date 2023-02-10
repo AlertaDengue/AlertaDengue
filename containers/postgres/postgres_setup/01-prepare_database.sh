2c84422b03693a5b3bd81ec9b08cb219637cb19e
Sandro Loch <es.loch@gmail.com>
2023-02-10 15:46:28 -0300

    fix(postgres): Add script to create schemas

--- containers/postgres/postgres_setup/01-prepare_database.sh
+++ containers/postgres/postgres_setup/01-prepare_database.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
+
+PSQL_CMD="psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER""
+
 # PostgreSQL databases
-echo "CREATE DATABASE ""$PSQL_DB"" WITH OWNER '""$PSQL_USER""';" | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"
-echo "CREATE DATABASE ""$PSQL_DBF"" WITH OWNER '""$PSQL_USER""';" | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"
+for dbname in "$PSQL_DB" "$PSQL_DBF"; do
+
+    echo "CREATE DATABASE "${dbname}" WITH OWNER '""$PSQL_USER""';" | ${PSQL_CMD}
+ 
+done
 
 # PostgreSQL roles
-echo "ALTER ROLE ""$PSQL_USER"" WITH PASSWORD '""$PSQL_PASSWORD""';" | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"
-echo "ALTER ROLE administrador WITH PASSWORD '""$PSQL_PASSWORD""';" | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"
-echo "ALTER ROLE forecast WITH PASSWORD '""$PSQL_PASSWORD""';" | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER"
+for dbusers in "$DB_ROLES"; do
+
+    echo "ALTER ROLE ""$PSQL_USER"" WITH PASSWORD '""$PSQL_PASSWORD""';" | ${PSQL_CMD}
+ 
+done
