f65108011ef5ef74055786a8d59400a991f15f5d
Sandro Loch <es.loch@gmail.com>
2023-02-15 09:44:50 -0300

    chore(docker): Fix conda build

--- containers/postgres/setup/01-prepare_database.sh
+++ containers/postgres/setup/01-prepare_database.sh
@@ -7,9 +7,9 @@ PSQL_CMD="psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER""
 # PostgreSQL databases
 
 for dbname in "${PSQL_DB}" "${PSQL_DBF}"; do
-    # CREATE DATABASE dengue WITH OWNER dengueadmin ENCODING 'utf-8';
-    echo "SELECT 'CREATE DATABASE "${dbname}" WITH OWNER "${PSQL_USER}"' WHERE NOT EXISTS (
-      SELECT FROM pg_database WHERE datname = '"${dbname}"')\gexec" | ${PSQL_CMD}
+    echo "CREATE DATABASE "${dbname}"
+        WITH OWNER "${PSQL_USER}"
+        ENCODING 'UTF8';" | ${PSQL_CMD}
 done
 
 # PostgreSQL roles
