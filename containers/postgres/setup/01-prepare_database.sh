a4d54f0ffefee366e734c79478c39c8f62a0ae2b
Sandro Loch <es.loch@gmail.com>
2023-02-15 10:25:50 -0300

    fix(postgres): Fix encoding

--- containers/postgres/setup/01-prepare_database.sh
+++ containers/postgres/setup/01-prepare_database.sh
@@ -7,9 +7,11 @@ PSQL_CMD="psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER""
 # PostgreSQL databases
 
 for dbname in "${PSQL_DB}" "${PSQL_DBF}"; do
-    echo "CREATE DATABASE "${dbname}"
+   echo "SELECT 'CREATE DATABASE "${dbname}" 
         WITH OWNER "${PSQL_USER}"
-        ENCODING 'UTF8';" | ${PSQL_CMD}
+        ENCODING "UTF8"' WHERE NOT EXISTS (
+      SELECT FROM pg_database WHERE datname = '"${dbname}"')\gexec" | ${PSQL_CMD}
+
 done
 
 # PostgreSQL roles
