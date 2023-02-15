a9db3bb9b716b6aa64e83977c31b727dc0413662
Sandro Loch <es.loch@gmail.com>
2023-02-15 07:36:44 -0300

    fix(docker): Give access to the dev user

--- containers/postgres/setup/02-create_dengue_db.sh
+++ containers/postgres/setup/02-create_dengue_db.sh
@@ -2,12 +2,15 @@
 
 set -e
 
+PSQL_CMD="psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER""
+
 if [[ "${ENV}" = "prod" ]]; then
-    echo "[II] restore from dump to production..."
-    gunzip -c /dumps/latest_dengue.sql.gz | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=dengue
-    gunzip -c /dumps/latest_infodengue.sql.gz | psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=infodengue
+    echo "[II] restore database from dump to production..."
+    gunzip -c /dumps/latest_dengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DB}
+    gunzip -c /dumps/latest_infodengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DBF}
 else
-    echo "[II] running script with schemas for demo database"
-    psql -d dengue < /schemas/02-dengue_schema.sql
-    ./schemas/03-grant-user-readonly.sh
+    echo "[II] running script with schemas for demo database."
+    psql -d ${PSQL_DB} < /schemas/schemas_dengue.sql
+    echo "[II] giving access to the dev user."
+    ./schemas/grant-user-readonly.sh
 fi
