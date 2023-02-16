c1e42fc9323206614453f8f07d6f12c292d67bb6
Sandro Loch <es.loch@gmail.com>
2023-02-16 12:00:42 -0300

    chore(docker): Change volumen

--- containers/postgres/setup/02-create_dengue_db.sh
+++ containers/postgres/setup/02-create_dengue_db.sh
@@ -4,13 +4,13 @@ set -e
 
 PSQL_CMD="psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER""
 
-if [[ "${DATABASE}" = "dumps" ]]; then
-    echo "[II] restore database from ${DATABASE} to production..."
-    gunzip -c /PG_RESTORE/latest_dengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DB}
-    gunzip -c /PG_RESTORE/latest_infodengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DBF}
+if [[ "${PG_RESTORE}" = "dumps" ]]; then
+    echo "[II] restore database from ${PG_RESTORE} to production..."
+    gunzip -c /"${PG_RESTORE}"/latest_dengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DB}
+    gunzip -c /"${PG_RESTORE}"/latest_infodengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DBF}
 else
-    echo "[II] creating ${DATABASE} for the demo database."
-    psql -d ${PSQL_DB} < /PG_RESTORE/schemas_dengue.sql
+    echo "[II] creating ${PG_RESTORE} for the demo database."
+    psql -d ${PSQL_DB} < /"${PG_RESTORE}"/schemas_dengue.sql
     echo "[II] giving access to the dev user."
-    ./PG_RESTORE/grant-user-readonly.sh
+    ./"${PG_RESTORE}"/grant-user-readonly.sh
 fi
