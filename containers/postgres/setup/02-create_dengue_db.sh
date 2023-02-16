6011918dc5dcd307f301664ae102c542a3cc3b76
Sandro Loch <es.loch@gmail.com>
2023-02-16 11:49:03 -0300

    feat(dockerfile): Add option to choose database restore type

--- containers/postgres/setup/02-create_dengue_db.sh
+++ containers/postgres/setup/02-create_dengue_db.sh
@@ -4,13 +4,13 @@ set -e
 
 PSQL_CMD="psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER""
 
-if [[ "${ENV}" = "prod" ]]; then
-    echo "[II] restore database from dump to production..."
-    gunzip -c /dumps/latest_dengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DB}
-    gunzip -c /dumps/latest_infodengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DBF}
+if [[ "${DATABASE}" = "dumps" ]]; then
+    echo "[II] restore database from ${DATABASE} to production..."
+    gunzip -c /PG_RESTORE/latest_dengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DB}
+    gunzip -c /PG_RESTORE/latest_infodengue.sql.gz | ${PSQL_CMD} -d ${PSQL_DBF}
 else
-    echo "[II] running script with schemas for demo database."
-    psql -d ${PSQL_DB} < /schemas/schemas_dengue.sql
+    echo "[II] creating ${DATABASE} for the demo database."
+    psql -d ${PSQL_DB} < /PG_RESTORE/schemas_dengue.sql
     echo "[II] giving access to the dev user."
-    ./schemas/grant-user-readonly.sh
+    ./PG_RESTORE/grant-user-readonly.sh
 fi
