a7684cc682614268c9b5ab9bdfde460ec57b2d92
Sandro Loch <es.loch@gmail.com>
2023-02-22 13:39:35 -0300

    chore(environment): Use the PSQL_DEV_USER variable in the script

--- containers/scripts/grant-roles-readonly.sh
+++ containers/scripts/grant-roles-readonly.sh
@@ -5,6 +5,6 @@ set -e
 PSQL_CMD="psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER""
 
 for schema in '"Dengue_global"' '"Municipio"' forecast public; do
-    echo "GRANT USAGE ON SCHEMA "${schema}" TO "infodenguedev";" | ${PSQL_CMD} -d ${PSQL_DB}
-    echo "GRANT SELECT ON ALL TABLES IN SCHEMA "${schema}" TO "infodenguedev";" | ${PSQL_CMD} -d ${PSQL_DB}
+    echo "GRANT USAGE ON SCHEMA "${schema}" TO "${PSQL_DEV_USER}";" | ${PSQL_CMD} -d ${PSQL_DB}
+    echo "GRANT SELECT ON ALL TABLES IN SCHEMA "${schema}" TO "${PSQL_DEV_PASSWORD}";" | ${PSQL_CMD} -d ${PSQL_DB}
 done
