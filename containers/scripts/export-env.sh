efe8eabed1df9be887eeb53f2220436b4b2382e6
Sandro Loch <es.loch@gmail.com>
2023-04-05 11:21:58 -0300

    chore(scripts): Move health-check script to scripts directory

--- containers/scripts/export-env.sh
+++ containers/scripts/export-env.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-## Usage:
-##   . ./export-env.sh ; $COMMAND
-
-unamestr=$(uname)
-if [ "$unamestr" = 'Linux' ]; then
-
-  export $(grep -v '^#' .env | xargs -d '\n')
-
-fi
-
 exec envsubst < .env_episcanner.tpl > /opt/services/AlertaDengue/.env
--- containers/scripts/run_scripts.sh
+++ containers/scripts/run_scripts.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo -n "[II] configuring crontab for the deploy user... "
+echo -e "[II] configuring crontab for the deploy user..."
 sudo bash /opt/services/scripts/crontab_install.sh
 
-echo -n "[II] exporting environment variables... "
+echo -e "[II] exporting environment variables..."
 bash /opt/services/scripts/export-env.sh
 
-echo -n "[II] collecting staticfiles and starting django... "
+echo -e "[II] Starting django..."
 bash /opt/services/AlertaDengue/runwsgi.sh
