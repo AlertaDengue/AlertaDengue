59847bfe9645fdb115f7542077c537d96d8c55b3
Luã Bida Vacaro <luabidaa@gmail.com>
2023-06-06 16:00:54 -0300

    Solving conflicts

---
+++ AlertaDengue/dados/templatetags/searchbox_component.py
@@ -0,0 +1,27 @@
+from dados.dbdata import STATE_NAME, RegionalParameters
+from dados.models import City
+from django import template
+
+# from ad_main import settings
+# from django.core.cache import cache
+
+register = template.Library()
+
+
+@register.inclusion_tag(
+    "components/searchbox/searchbox.html", takes_context=True
+)
+def searchbox_component(context):
+    options_cities = []
+    for state_name in STATE_NAME.values():
+        for (
+            geocode,
+            city_name,
+        ) in RegionalParameters.get_cities(state_name=state_name).items():
+            options_cities.append(City(geocode, city_name, state_name))
+
+    context = {
+        "options_cities": options_cities,
+    }
+
+    return context
