b853f54069b658171871d0eaf39fb3bcf7e7ad85
Luã Bida Vacaro <luabidaa@gmail.com>
2023-06-06 16:06:45 -0300

    # This is a combination of 7 commits. # This is the 1st commit message:
    fix(searchbox): improve searchbox speed and recognition
    
    # The commit message #2 will be skipped:
    
    # fix(searchbox): improve search on nav bar
    
    # The commit message #3 will be skipped:
    
    # Clean leftovers and fix translation
    
    # The commit message #4 will be skipped:
    
    # Trying to solve CI
    
    # The commit message #5 will be skipped:
    
    # Cache municipalities and fix maps query
    
    # The commit message #6 will be skipped:
    
    # Minor fix
    
    # The commit message #7 will be skipped:
    
    # Adding back EMAIL_BACKEND on CI

--- AlertaDengue/dados/templatetags/searchbox_component.py
+++ AlertaDengue/dados/templatetags/searchbox_component.py
@@ -1,6 +1,8 @@
 from dados.dbdata import STATE_NAME, RegionalParameters
 from dados.models import City
 from django import template
+from django.conf import settings
+from django.core.cache import cache
 
 register = template.Library()
 
@@ -9,6 +11,15 @@ register = template.Library()
     "components/searchbox/searchbox.html", takes_context=True
 )
 def searchbox_component(context):
+    cache_name = "options_cities"
+    res = cache.get(cache_name)
+
+    if res:
+        context = {
+            "options_cities": res,
+        }
+        return context
+
     options_cities = []
     for state_name in STATE_NAME.values():
         for (
@@ -16,6 +27,12 @@ def searchbox_component(context):
             city_name,
         ) in RegionalParameters.get_cities(state_name=state_name).items():
             options_cities.append(City(geocode, city_name, state_name))
+
+    cache.set(
+        cache_name,
+        options_cities,
+        settings.QUERY_CACHE_TIMEOUT,
+    )
 
     context = {
         "options_cities": options_cities,
