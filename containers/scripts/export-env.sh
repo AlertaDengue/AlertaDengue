1dcd2fbb0db5017de3804923cfe7320fc1debb51
Sandro Loch <es.loch@gmail.com>
2023-06-06 13:30:11 -0300

    feat: Remove Episcanner-downloader (#641)
    * feat(workflow): Remove Episcanner-downloader
    
    * chore(deps): Pin urllib3 and Django version
    
    * Minor changes

--- containers/scripts/export-env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec envsubst < .env_episcanner.tpl > /opt/services/AlertaDengue/.env
--- containers/scripts/run_scripts.sh
+++ containers/scripts/run_scripts.sh
@@ -3,8 +3,5 @@
 echo -e "[II] configuring crontab for the deploy user..."
 sudo bash /opt/services/scripts/crontab_install.sh
 
-echo -e "[II] exporting environment variables..."
-bash /opt/services/scripts/export-env.sh
-
 echo -e "[II] Starting django..."
 bash /opt/services/AlertaDengue/runwsgi.sh
