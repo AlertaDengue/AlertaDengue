67e891b225199da24bb01d1f1b6c7c358ff29238
Sandro Loch <es.loch@gmail.com>
2023-06-13 14:33:54 -0300

    feat(docker): Add compose-dev

--- containers/scripts/services_run.sh
+++ containers/scripts/services_run.sh
@@ -4,7 +4,7 @@
 set +ex
 
 echo "[INFO] Starting collectstatics..."
-python3 manage.py collectstatic --noinput
+python3 /opt/services/AlertaDengue/manage.py collectstatic --noinput
 
 exec "$@" &
 . /opt/services/celery.sh
