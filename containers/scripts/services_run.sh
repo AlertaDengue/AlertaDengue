494c56415ada4b6b9c634b3c6ee694ca6620cfcf
Sandro Loch <es.loch@gmail.com>
2023-06-13 13:20:58 -0300

    fix(docker): Update Dockerfile and compose to start Celery from scripts

--- containers/scripts/services_run.sh
+++ containers/scripts/services_run.sh
@@ -3,9 +3,8 @@
 
 set +ex
 
+echo "[INFO] Starting collectstatics..."
 python3 manage.py collectstatic --noinput
 
-exec gunicorn -w 4 -b 0.0.0.0:8000 ad_main.wsgi:application --timeout 9640 &
-
-. /opt/services/crontab_setup.sh &
-. /opt/services/celery.sh 
+exec "$@" &
+. /opt/services/celery.sh
