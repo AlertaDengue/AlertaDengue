2fb508a73bc25062cc31c5eef3f127b1c6541084
Sandro Loch <es.loch@gmail.com>
2023-06-14 14:33:35 -0300

    feat(workflow): Improve makim and sugar config file

--- tests/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd ${current_dir}; cd ..
-env_file=".env" /opt/services/AlertaDengue/manage.py test ${1}
