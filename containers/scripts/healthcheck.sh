ae2f5c6aad5702a9594c9d750db0166bd4a479f8
Sandro Loch <es.loch@gmail.com>
2023-11-22 05:22:21 -0300

    feat: Standardize Docker compose configuration and Update Sugar and Makim specs (#678)
    * feat: Remove Minio service from compose file
    
    * fix: Add rabbitmq in the infodengue network
    
    * chore: Standardize service names and set container names in docker compose file
    
    * fix(ci): Add pgdata for environment variables
    
    * Update configs for sugar yaml file
    
    * fix(docker): Add uid:gid for postgres Dockerfile
    
    * chore(ci): Change path for postgres_data
    
    * feat: Rename kxgr to sugar
    
    * fix: Update makim specs
    
    * refactor: Fix containers healthcheck and Change services name
    
    * Remove tmate

--- containers/scripts/healthcheck.sh
+++ containers/scripts/healthcheck.sh
@@ -2,20 +2,23 @@
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd ../ && pwd )"
 
-if [ -f ${PROJECT_DIR}/.env ]; then
+if [ -f "${PROJECT_DIR}/.env" ]; then
     # Load Environment Variables
-    export $(cat ${PROJECT_DIR}/.env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}' )
+    export $(grep -v '#' "${PROJECT_DIR}/.env" | sed 's/\r$//' | awk -F= '{print $1}' )
 fi
 
-export CONTAINER_NAME=${1:-""}
-export CONTAINER_NAME="infodengue-${ENV:-dev}_${CONTAINER_NAME}_1"
+CONTAINER_NAME=${1:-""}
+CONTAINER_NAME="infodengue-${ENV:-dev}-${CONTAINER_NAME}"
 
 echo "[II] Checking ${CONTAINER_NAME} ..."
 
-while [ "`docker inspect -f {{.State.Health.Status}} ${CONTAINER_NAME}`" != "healthy" ]
-do
-    echo "[II] Waiting for ${CONTAINER_NAME} ..."
-    sleep 5;
+while true; do
+    HEALTH_STATUS=$(docker inspect --format='{{.State.Health.Status}}' "${CONTAINER_NAME}" 2>/dev/null)
+    if [ "${HEALTH_STATUS}" = "healthy" ]; then
+        echo "[II] ${CONTAINER_NAME} is healthy."
+        break
+    else
+        echo "[II] Waiting for ${CONTAINER_NAME} ..."
+        sleep 5
+    fi
 done
-
-echo "[II] ${CONTAINER_NAME} is healthy."
