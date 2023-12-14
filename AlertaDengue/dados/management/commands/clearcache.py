646d275a247fb6ea19d2dcd490e34184c84cd0f7
Sandro Loch <es.loch@gmail.com>
2023-12-14 11:46:00 -0300

    feat: Introduce Django cache management and memcached service expose port (#688)
    * feeat: Expose port for memcached service in the compose yaml
    
    * feat: Add custom Django management command for clearing the cache

---
+++ AlertaDengue/dados/management/commands/clearcache.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.core.cache import caches
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = "Clear cache"
+
+    def handle(self, **options):
+        for k in settings.CACHES.keys():
+            caches[k].clear()
+            self.stdout.write("Cleared cache '{}'.\n".format(k))
