ca7c1879612090f649772628c87e206c8a3606f7
Luã Bida Vacaro <luabidaa@gmail.com>
2023-12-26 14:03:46 -0300

    Include episcanner dep

--- AlertaDengue/ad_main/celeryapp.py
+++ AlertaDengue/ad_main/celeryapp.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery
-from django.conf import settings
 
 # This makes sure celery knows where settings are, so we don't need to set it
 # when calling it from the command line
@@ -9,5 +8,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ad_main.settings")
 
 
 app = Celery("ad_main", include=["dbf.tasks"])
-app.config_from_object(settings, namespace="CELERY")
+app.config_from_object("ad_main.celery_settings", namespace="CELERY")
 app.autodiscover_tasks()
