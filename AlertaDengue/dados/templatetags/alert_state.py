0b441ecb6e8195e66b9cdaac483be956af3f6980
Sandro Loch <es.loch@gmail.com>
2024-02-01 15:20:49 -0300

    chore: Add new component and dummy data

--- AlertaDengue/dados/templatetags/alert_state.py
+++ AlertaDengue/dados/templatetags/alert_state.py
@@ -10,15 +10,31 @@ register = template.Library()
 @register.simple_tag
 def generate_altair_line_chart() -> str:
     """
-    Generates a standalone HTML representation of an Altair line chart.
+    Generates a standalone HTML representation of an Altair line chart using dummy data,
+    with three different stock symbols over varying periods.
 
-    Returns:
-        str: The HTML representation of the Altair line chart.
+    Returns
+    -------
+    str
+        The HTML representation of the Altair line chart.
     """
-    # Load the data into a DataFrame and then parse the date column
-    source_df = pd.DataFrame(vega_data.stocks())
-    source_df["date"] = pd.to_datetime(source_df["date"], format="%Y-%m-%d")
+    # Adjusted to match the total length of the combined symbol and price arrays
+    total_days = 51 + 100 + 200  # Total periods for all symbols
+    date_range = pd.date_range(
+        start="2020-01-01", periods=total_days, freq="D"
+    )
 
+    # Extend the 'symbol' and 'price' arrays to match the 'date' array length
+    data = {
+        "date": date_range,
+        "symbol": ["AAPL"] * 51 + ["MSFT"] * 100 + ["ESLOCH"] * 200,
+        "price": list(range(1, 52))
+        + list(range(1, 101))
+        + list(range(1, 201)),
+    }
+    source_df = pd.DataFrame(data)
+
+    # Generate the chart
     chart = (
         alt.Chart(source_df)
         .mark_line(interpolate="monotone")
@@ -50,4 +66,11 @@ def vis_altair(context: dict) -> dict:
             "altair_line_chart": altair_line_chart,
         }
     )
+    return context
+
+
+@register.inclusion_tag(
+    "components/alert_state/epi-years.html", takes_context=True
+)
+def epi_years(context: dict) -> dict:
     return context
