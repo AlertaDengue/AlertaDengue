0f92b90f84fd5b844df90a74e96c6d7a805f971a
Sandro Loch <es.loch@gmail.com>
2024-02-12 08:53:11 -0300

    fix: Add JS into template for AlertaStateView

--- AlertaDengue/static/js/report_type_script.js
+++
@@ -1,8 +0,0 @@
-// report_type_script.js
-function openReport() {
-    var state_initial = $('#state option:selected').val();
-    var report_type = $('#report-type option:selected').val();
-    window.location.href = '/report/' + state_initial + '/' + report_type;
-    return false;
-  }
-  
