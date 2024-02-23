d2a2bd2f8e83754dbec382bc93b3924f62e206c2
Luã Bida Vacaro <luabidaa@gmail.com>
2024-02-23 16:01:13 -0300

    unhardcode redis port

--- AlertaDengue/ad_main/celery_settings.py
+++ AlertaDengue/ad_main/celery_settings.py
@@ -4,6 +4,7 @@ from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ad_main.settings")
 
+REDIS_PORT = os.environ.get("REDIS_PORT")
 ENV = "dev" if settings.DEBUG else "prod"
 
 broker_url = f"redis://infodengue-{ENV}-redis:6379/0"
