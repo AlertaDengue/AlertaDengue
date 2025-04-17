5d6c7f8a68a1e75c2393e1a4be3f83fe60a1ba8a
Luã Bida Vacaro <luabidaa@gmail.com>
2025-04-17 14:14:42 -0300

    fix /api/notif_reduced error

--- AlertaDengue/dados/views.py
+++ AlertaDengue/dados/views.py
@@ -92,7 +92,7 @@ def hex_to_rgb(value):
     value = value.lstrip("#")
     lv = len(value)
     return tuple(
-        int(value[i: i + lv // 3], 16)
+        int(value[i : i + lv // 3], 16)
         for i in range(0, lv, lv // 3)  # noqa: E203
     )
 
