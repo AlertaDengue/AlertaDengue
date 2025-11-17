698772c9e8eb9736a3c2cfa6d4a7a2f32044fd84
esloch <es.loch@gmail.com>
2025-11-17 10:23:47 -0300

    feat(home): refactor layout with responsive banners and feature buttons

--- AlertaDengue/dados/templatetags/home_components.py
+++ AlertaDengue/dados/templatetags/home_components.py
@@ -28,25 +28,9 @@ def collapse_component(context: Dict[str, Any]) -> Dict[str, Any]:
     return context
 
 
-@register.inclusion_tag("components/home/carousel.html", takes_context=True)
-def carousel_component(context: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    Render the carousel component with the provided context.
-
-    Parameters
-    ----------
-    context : Dict[str, Any]
-        The context containing the data to be rendered.
-
-    Returns
-    -------
-    Dict[str, Any]
-        The updated context.
-
-    """
-    return context
-
-
+@register.inclusion_tag(
+    "components/home/banners_header.html", takes_context=True
+)
 @register.inclusion_tag("components/home/legend.html", takes_context=True)
 def legend_component(context: Dict[str, Any]) -> Dict[str, Any]:
     """
