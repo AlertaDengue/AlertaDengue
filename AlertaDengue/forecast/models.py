fc534230bdeb3d1fa546b36bca456bd1ef0f4edb
esloch <es.loch@gmail.com>
2026-03-02 17:50:08 -0300

    fix(import): Update lazy imports from getext

--- AlertaDengue/forecast/models.py
+++ AlertaDengue/forecast/models.py
@@ -1,7 +1,7 @@
 # from django.apps import apps
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # City = apps.get_model('dados', 'City')
 
