fd805f9e39bc80ed1ca5ccafcc21e5de7e80a4c6
Sandro Loch <es.loch@gmail.com>
2026-07-02 17:52:27 +0000

    refactor(gis): move geofile sync into dados

--- AlertaDengue/dados/geoutils.py
+++ AlertaDengue/dados/geoutils.py
@@ -3,7 +3,6 @@ from geopandas import GeoDataFrame
 
 
 def extract_boundaries(gdf: GeoDataFrame):
-    """ """
     bound_min = gdf.bounds[["minx", "miny"]].min()
     bound_max = gdf.bounds[["maxx", "maxy"]].max()
     bounds = np.array((bound_min, bound_max)).flatten()
