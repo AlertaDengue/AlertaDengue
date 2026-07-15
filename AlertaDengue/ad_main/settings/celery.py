00e8124ac87c901af7b53d7c3db5b53d219283e0
Sandro Loch <es.loch@gmail.com>
2026-07-15 13:02:48 -0300

    fix(types): resolve initial mypy baseline errors (#997)

--- AlertaDengue/ad_main/settings/celery.py
+++ AlertaDengue/ad_main/settings/celery.py
@@ -1,11 +1,14 @@
 import os
 
-DJANGO_SETTINGS_MODULE = os.environ.setdefault("DJANGO_SETTINGS_MODULE")
+DJANGO_SETTINGS_MODULE = os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    os.getenv("DJANGO_SETTINGS_MODULE", "ad_main.settings.dev"),
+)
 
 
-if DJANGO_SETTINGS_MODULE == "config.settings.dev":
+if DJANGO_SETTINGS_MODULE == "ad_main.settings.dev":
     from ad_main.settings.dev import *  # noqa: F403
-elif DJANGO_SETTINGS_MODULE == "config.settings.staging":
+elif DJANGO_SETTINGS_MODULE == "ad_main.settings.staging":
     from ad_main.settings.staging import *  # noqa: F403
 else:
     from ad_main.settings.prod import *  # noqa: F403
