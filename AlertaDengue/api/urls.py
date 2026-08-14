e15bb5012eb35371e65d9028715a5d13c1d47493
Sandro Loch <es.loch@gmail.com>
2026-08-14 10:03:56 +0000

    feat(api): expose historical alert endpoint

--- AlertaDengue/api/urls.py
+++ AlertaDengue/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path, re_path
 
+from .internal.views import HistoricalAlertListView
 from .views import AlertCityView, EpiYearWeekView, NotificationReducedCSV_View
 
 app_name = "api"
@@ -15,6 +16,11 @@ urlpatterns = [
         r"^epi_year_week$",
         EpiYearWeekView.as_view(),
         name="epi_year_week",
+    ),
+    path(
+        "historical-alerts/",
+        HistoricalAlertListView.as_view(),
+        name="historical_alerts",
     ),
     path("internal/", include("api.internal.urls")),
 ]
